f1ad3553e201e0f7f7a0dee1c91eff4ddf9b60b5
Nico <jngp1980@gmail.com>
2024-12-02 19:40:33 +0100

    Tarea UD3 Nuevo Usuario

---
+++ www/UD3/anexos/entregaTarea/pdo.php
@@ -0,0 +1,31 @@
+<?php 
+function conecta($servername, $dbname, $username, $userpass){
+    $conexion = new PDO("mysql.host=$servername; dbname=$dbname",$username,$userpass);
+    return $conexion;
+}
+
+function nuevoUser($id,$username,$nombre,$apellidos,$contrasena){
+    try{
+        $conexion = conecta('db', 'usuarios', 'root', 'test');
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql= "INSERT INTO usuarios (id, username, nombre, apellidos, contrasena) 
+        VALUES (:id, :username, :nombre, :apellidos, :contrasena)";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+        $stmt->bindParam(':apellidos', $apellidos, PDO::PARAM_STR);
+        $stmt->bindParam(':contrasena', $contrasena, PDO::PARAM_STR);
+
+        return $stmt->execute();
+
+    }catch(PDOException $e){
+        error_log("Error al insertar el usuario: " . $e->getMessage());
+        return false; 
+    }finally{
+        $conexion = null;
+    }
+
+}
+
+?>
