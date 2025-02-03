c39f9e00bb932785b958fa7444e7192656d40360
Nico <a24josegp@iessanclemente.net>
2025-02-03 23:41:35 +0100

    Nuevo usuario param miss

--- www/UD4/entregaTarea/usuarios/nuevoUsuario.php
+++ www/UD4/entregaTarea/usuarios/nuevoUsuario.php
@@ -56,7 +56,7 @@
                         if (!$error)
                         {
                             require_once('../modelo/pdo.php');
-                            $resultado = nuevoUsuario(filtraCampo($nombre), filtraCampo($apellidos), filtraCampo($username), $contrasena);
+                            $resultado = nuevoUsuario(filtraCampo($nombre), filtraCampo($apellidos), filtraCampo($username), $rol, $contrasena);
                             if ($resultado[0])
                             {
                                 echo '<div class="alert alert-success" role="alert">Usuario guardado correctamente.</div>';
