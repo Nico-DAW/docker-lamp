46ba937c2e21fbffbde050ca261bd9aebb4dfb13
Nico <jngp1980@gmail.com>
2025-02-18 14:41:46 +0100

    Clase usuario - Tarea

---
+++ www/UD5/entregaTarea/usuarios/usuario.php
@@ -0,0 +1,67 @@
+<?php 
+class Usuario{
+    private $id;
+    private $username;
+    private $nom;
+    private $apel;
+    private $pass;
+    private $rol; 
+
+    public function setId($id){
+        $this->id = $id; 
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setUsername($username){
+        $this->username = $username; 
+    }
+
+    public function getUsername(){
+        return $this->username;
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom; 
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function setApel($apel){
+        $this->apel = $apel; 
+    }
+
+    public function getApel(){
+        return $this->apel;
+    }
+
+    public function setPass($pass){
+        $this->pass = $pass;
+    }
+
+    public function getPass(){
+        return $this->pass;
+    }
+
+    public function setRol($rol){
+        $this->rol = $rol;
+    }
+
+    public function getRol(){
+        return $this->rol;
+    }
+
+    public function __construct($id, $username, $nom, $apel, $pass, $rol){
+        $this->id = $id;
+        $this->username = $username;
+        $this->nom = $nom;
+        $this->apel = $apel;
+        $this->pass = $pass;
+        $this->rol = $rol;
+    }
+}
+?>
