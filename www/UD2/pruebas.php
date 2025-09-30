a9d2442f71d4f20805aa5c6d97c99189df82b6e0
Nico <jngp1980@gmail.com>
2025-09-30 11:50:55 +0200

    Ejercicios UD2

---
+++ www/UD2/pruebas.php
@@ -0,0 +1,15 @@
+<?php
+    echo "<h1>¡Bienvenido!</h1>";         
+    /*
+        $array = array(
+            1    => "a",
+            "1"  => "b",
+            1.5  => "c",
+        true => "d",
+    );
+    */
+    //var_dump($array);
+
+    $_N=1;
+    echo $_N;
+?>            
