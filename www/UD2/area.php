d87e181dd7a28eb3d4bf184b1f763d2d4ac2b4ff
Nico <jngp1980@gmail.com>
2025-09-30 14:39:27 +0200

    Primeros ejercicios 30/09/2025

---
+++ www/UD2/area.php
@@ -0,0 +1,12 @@
+<?php 
+$base=20;
+$altura=10;
+
+$area=$base*$altura;
+
+echo $area."<br>"; 
+
+$perimetro=2*$base+2*$altura;
+
+echo $perimetro; 
+?>
--- www/UD2/pruebas.php
+++ www/UD2/pruebas.php
@@ -11,5 +11,52 @@
     //var_dump($array);
 
     $_N=1;
-    echo $_N;
+    echo $_N."<br/>";
+
+    $a="true";
+
+    echo is_bool($a);
+    //No imprime nada porque... 
+    //is_bool($a) returns false because $a is a string, not a boolean.
+    //false is printed as an empty string when used with echo, not "0".
+
+    $b=0;
+    echo is_bool($b);
+
+    if($b){
+        echo "Bananas!";
+    }
+
+    //Aquí tampoco imprime nada. Si se cambia a 1 el valor de $b imprime Bananas!
+
+    $c="false"; 
+    echo gettype($c)."<br>";
+
+    //devuelve string
+    $d="";
+    echo empty($d)."<br>";
+    //deuelve 1 porque es true
+
+    //PHP considera los siguientes valores como "vacíos":
+
+    /*
+    ""	Cadena vacía
+    0	Entero cero
+    "0"	Cadena con un cero
+    0.0	Número flotante cero
+    NULL	Nulo
+    false	Booleano falso
+    array()	Array vacío
+    */
+
+    $e = 0.0; // el valor devuelto por empty($e);
+    // $f = 0; // el valor devuelto por empty($f);
+    // $g = false; // el valor devuelto por empty($g);
+    // $h; // el valor devuelto por empty($h);
+    // $i = “0”; // el valor devuelto por empty($i);
+    // $j = “0.0”; // el valor devuelto por empty($j);
+    // false, null, 0 y cadena vacia son considerados empty --> true - imprimirá 1 
+    //recordar que //false is printed as an empty string when used with echo, not "0".
+
+    echo empty($e);
 ?>            
