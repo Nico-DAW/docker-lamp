a0e4057582c919e11f5740fc5511075e089ccb22
Nico <jngp1980@gmail.com>
2025-10-15 16:30:19 +0200

    anexo checkpoint

---
+++ www/UD2/anexo/header.php
@@ -0,0 +1,3 @@
+<header class="bg-primary text-white text-center py-3">
+    UD2 - Nicolás García Peñalver
+</header>
---
+++ www/UD2/anexo/menu.php
@@ -0,0 +1,21 @@
+<nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+    <div class="position-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="">
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">
+                    Mis tareas
+                </a>
+             <li class="nav-item">
+                <a class="nav-link" href="">
+                    Nueva tarea
+                </a>
+            </li>
+            </li>
+        </ul>
+    </div>
+</nav>
