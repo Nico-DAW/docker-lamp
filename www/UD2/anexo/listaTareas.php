8d8eab196ca07f8f702d1cf621623365b59af6be
Nico <jngp1980@gmail.com>
2025-10-17 11:32:52 +0200

    Listado Tareas MVC

--- www/UD2/anexo/listaTareas.php
+++ www/UD2/anexo/listaTareas.php
@@ -15,7 +15,7 @@
             <?php include_once("menu.php");?>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h2>Nueva Tarea</h2>
+                    <h2>Lista Tareas</h2>
                 </div>
                 <div class="container">
                     <p></p>
@@ -29,8 +29,14 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php include_once('utils.php') ?>
-                                    <?php listarTareas($tareasarr);?>
+                                <?php include_once('utils.php');
+                                        $tareas=listarTareas($tareasarr);
+
+                                      foreach($tareas as $key=>$value){
+                                        echo "<tr><td>".$key."</td><td>".$value[0]."</td><td>".$value[1]."</td></tr>";
+                                      }
+
+                                ?>
                             </tbody>
                         </table>
                     </div>
--- www/UD2/anexo/utils.php
+++ www/UD2/anexo/utils.php
@@ -5,10 +5,20 @@ $tareasarr=[
     ["Funciones y Librerias. Ejercicios de funciones y librerias","En curso"]
 ];
 
+/*
+A ver... esto no es... elegante... se mezcla vista y controlador. Lo que se tenga que mostrar hacerlo en la vista. Separado del controlador.
+
 function listarTareas($tareasarr){
     foreach($tareasarr as $key=>$value){
         echo "<tr><td>".$key."</td><td>".$value[0]."</td><td>".$value[1]."</td></tr>";
     }
 };
+
+Por tanto-->
+*/
+
+function listarTareas($tareasarr){
+    return $tareasarr;
+}
 
 ?>
