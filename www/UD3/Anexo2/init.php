d2c02f8976e405d44a93f845d6e71b7a241dd271
Nico <jngp1980@gmail.com>
2025-11-17 19:13:38 +0100

    UD3-Anexo2-Init1

---
+++ www/UD3/Anexo2/init.php
@@ -0,0 +1,39 @@
+<?php 
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+function conecta(){
+    $servername = "db";
+    $username = "root";
+    $password = "test";
+    $conexion = new mysqli($servername, $username, $password);
+
+    if($conexion->connect_error){
+        die("Se ha producido un error al intentar crear la BBDD ".$conexion->connect_error);
+    }
+
+    return $conexion; 
+}
+
+/*
+
+Para poder utilizar excepciones en Mysqli es necesario habilitarlas:
+Si no se activan de la siguiente manera->
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+entonces try/catch NO funciona, porque las funciones de MySQLi no lanzan excepciones por defecto.
+
+La activación se realizará al inicio del script -> <?php ya que se activarán las excepciones del forma global. 
+
+*/
+
+function creaDB(){
+    $conexion = conecta(); 
+    $sql = "CREATE DATABSE IF NOT EXISTS tareas";
+    try{
+        $conexion->query($sql);
+        echo "Base de datos creada correctamente";
+    }catch(mysqli_sql_exception $e){
+        echo "Error: ".$e->getMessage();
+    }
+}
---
+++ www/UD3/Anexo2/listaTareas.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UD2. Nueva Tarea</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <?php include_once("header.php");?>
+    <!--header-->
+    <div class="container-fluid">
+        <div class="row">
+            <!--menu-->
+            <?php include_once("menu.php");?>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h2>Lista Tareas</h2>
+                </div>
+                <div class="container">
+                    <p></p>
+                    <div class="table">
+                        <table class="table table-striped table-hover">
+                            <thead class="thead">
+                                <tr>                            
+                                    <th>Identificador</th>
+                                    <th>Descripción</th>
+                                    <th>Estado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php include_once('utils.php');
+                                        $tareas=listarTareas($tareasarr);
+
+                                      foreach($tareas as $key=>$value){
+                                        echo "<tr><td>".$key."</td><td>".$value[0]."</td><td>".$value[1]."</td></tr>";
+                                      }
+
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    </br>
+                </div>
+            </main>
+        </div>
+    </div>
+    <!--footer-->
+    <?php include_once("footer.php");?>
+</body>
+</html>
