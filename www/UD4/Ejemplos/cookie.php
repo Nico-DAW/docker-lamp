030954ea7ba96887e2a8951f280aa078a557340b
Nico <jngp1980@gmail.com>
2026-01-23 14:52:54 +0100

    Inicio UD4

---
+++ www/UD4/Ejemplos/cookie.php
@@ -0,0 +1,16 @@
+<?php 
+$nombre = "Primera";
+$value = 1; 
+setcookie($nombre, $value,time()+360);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
