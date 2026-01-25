82e6b7cedd1632b9eb6681969b98f60bc3c5a4ef
Nico <nico@mail.com>
2026-01-25 21:55:41 +0100

    Crear y añadir a un archivo

--- www/UD4/Ejemplos/crearAchivo.php
+++ www/UD4/Ejemplos/crearAchivo.php
@@ -12,8 +12,22 @@
     la va a borrar. Vemos un ejemplo
     */
 
-    $archivo = fopen("Ejemplo.txt","w");
-    // Aquí el archivo Ejemplo.txt no existe icialmente por lo que se crea. 
+     $archivo = fopen("Ejemplo.txt","w") or die("Unable to open file");
+    /* Aquí el archivo Ejemplo.txt no existe icialmente por lo que se crea. 
+    La carpeta en la que se vaya a crear el archivo tiene que tener permisos de escritura -->$ sudo chmod 777 Ejemplos
+    Ahora podriamos escribir información en el archivo con fwrite() --> 
+    */
+    fwrite($archivo, "Sabela\nJuan");
+    fclose($archivo);
+    /* El caracter de escape aquí es /n para generar el espacio
+    En el caso de volver a hace un fwrite --> por ejemplo fwrite($archivo, "Alvaro\nMaria");
+    Se caragaría la información anterior. Para añadir información a un archivo empleamos a en fopen("Ejemplo.txt","a")
+    */
+    
+    $archivo = fopen("Ejemplo.txt","w") or die("Unable to open file");
+    fwrite($archivo, "Alvaro\nMaria");
+    fclose($archivo);
     ?>
+    <h2>Hello!</h2>
 </body>
 </html>
