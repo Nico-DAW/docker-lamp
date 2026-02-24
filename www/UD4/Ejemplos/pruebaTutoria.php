25a259c9699d355d1d1f52a53b5f3a722f2fd263
Nico <nico@mail.com>
2026-02-24 19:20:24 +0100

    cookies basico

--- www/UD4/Ejemplos/pruebaTutoria.php
+++ www/UD4/Ejemplos/pruebaTutoria.php
@@ -2,8 +2,26 @@
 $cookiename = "usuario";
 $cookievalue = "Sabela"; 
 
-setcookie($cookiename, $cookievalue, time()+3600, "/");
+//setcookie($cookiename, $cookievalue, time()-360, "/");
+
+/*
+A continuación no podríamos emplear tampoco el siguiente código, 
+ya que generaríamos un bucle infinito. 
+
+header("Location:".$_SERVER['PHP_SELF']);
+exit();
+
+Habrá que emplear un condicional: 
+*/
+
+if (!isset($_COOKIE[$cookiename])) {
+    setcookie($cookiename, $cookievalue, time()+3600, "/");
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +30,28 @@ setcookie($cookiename, $cookievalue, time()+3600, "/");
     <title>Document</title>
 </head>
 <body>
+    <?php
+    if(!isset($_COOKIE[$cookiename])){
+        echo "La cookie no está definida";
+    }else{
+        echo "La cookie ".$_COOKIE[$cookiename]." está definida";
+    } 
+    /*
+    Las cookies no están disponibles justo en el momento de ser seteadas: setcookie(...)
+    es necesario recargar la página para que estas estén disponibles. Es habitual 
+    el empleo de header("Location:") --> exit() para recargar la paǵina.
+    */
+
+    /* 
+
+    header("Location:".$_SERVER['PHP_SELF']);
+    exit();
+    
+    En este punto no podríamos crear un header("Location:...") daría error
+    Siempre que empleemos header(), setcookie(), session_start(). Deben ir antes de cualquier 
+    echo o HTML.
+    */
+    ?>
     
 </body>
 </html>
