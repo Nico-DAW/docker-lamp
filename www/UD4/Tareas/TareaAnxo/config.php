54d3ad5f645b26475e57bfaefc2f9c7f65b410d2
Nico <nico@mail.com>
2026-02-26 19:04:22 +0100

    Tarea Anxo UD4-1

---
+++ www/UD4/Tareas/TareaAnxo/config.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+Archivo de configuración --> Dispondremos aquí el código que en principio
+necesitaremos en todas las páginas. Como por ejemplo session_start()
+*/
+
+//Iniciamos la sesion la idea será incluir este archivo al inicio de todos los ficheros. 
+
+session_start();
+
+/* 
+Definimos los posibles usuarios en un array. Un simulación. Lo suyo sería recuperarlos
+de un BBDD.
+*/
+$usuarios = [
+    'raquel'=>['clave'=>'1234.abc', 'rol'=>'player'],
+    'paco'=>['clave'=>'abc.1234', 'rol'=>'admin']
+];
+
+?>
---
+++ www/UD4/Tareas/TareaAnxo/login.php
@@ -0,0 +1,24 @@
+<?php 
+require_once('config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Página de Login</h2>
+    
+    <!-- Comprobaciones
+    <?php
+    foreach($usuarios as $key=>$value){
+        echo $key.'->';
+        echo $value['rol'].'<br>';
+    }
+    ?>
+    -->
+    
+</body>
+</html>
