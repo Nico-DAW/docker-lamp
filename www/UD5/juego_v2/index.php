26323fbd3088fbbc17ec8d9ed5b86f2f40b93a9d
Nico <nico@mail.com>
2026-03-10 22:09:54 +0100

    juego logout

--- www/UD5/juego_v2/index.php
+++ www/UD5/juego_v2/index.php
@@ -8,8 +8,34 @@ compruebaSesion();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+        header{
+            background-color: #ffae16;
+        }
+
+        nav{
+            display:flex;
+            justify-content: center;
+        }
+
+        nav ul{
+            display: flex;
+            gap:15px;
+            list-style: none;
+        }
+
+        nav ul li a{
+            text-decoration: none;
+            color: #ffff;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
+    <?php require_once("views/header.php");?>
     <h2>Mis juegos</h2>
     <hr>
     <p>Dasboard juegos</p>
